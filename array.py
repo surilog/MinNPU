9612f1655d808040c90573b95c3932adbf30162c
yanghwan <jamgyang@gmail.com>
2026-08-06 16:46:32 +0900

    클래스 분류화 및 구조 정리

--- array.py
+++ array.py
@@ -14,143 +14,151 @@ class Matrix:
         print(self.matrix)
         
         print(self.n)"""
+
+    def mac(self, pathern: 'Matrix') -> float | None:
+            if self.n != pathern.n:
+                print(f" 오류 : 행렬 크기가 맞지 않습니다. ({self.n}*{self.n}) VS ({pathern.n}*{pathern.n})")
+                return None
+            if not self.data or not pathern.data:
+                print("오류 : 행렬 데이터가 없습니다. ")
+                return None
     
+            #제너레이터를 쓰면 0부터 더해서 초기화 필요 X
+            total_sum = sum(
+                self.data[r][c] * pathern.data[r][c]
+                for r in range(self.n)
+                for c in range(self.n)
+            )
+            """total_sum= sum(self.data[r][c] * pathern.data[r][c]for r in range(self.n)for c in range(self.n))"""
+            """
+            total_sum = 0.0
+            
+            for r in range(self.n):
+                for c in range(self.n):
+                    total_sum += self.data[r][c] * pathern.data[r][c]"""
+            return float(total_sum)
 
     """  
     def get_val(self, r: int, c: int) -> float:
         return self.data[r][c] #(r,c) 위치 반환
 
     def set_val(self, r:int, c:int, val : float):
-        self.data[r][c] = val# (r,c)위치에 값 저장"""
+        self.data[r][c] = val# (r,c)위치에 값 저장
+        """
 
     def display(self):
-        print(f"[{self.n}x{self.n}  Matrix]")
+        print(f"[{self.n}x{self.n}  ")
         for row in self.data:
             print(row)
 
-    def user_input(self, name:str = "패턴") -> bool:
-        print(f"\n[{name} 입력 ({self.n}x{self.n})]")
-        print(f"아래에 {self.n}줄의 데이터를 한 번에 입력(또는 붙여넣기) 후, 엔터를 한 번 더 눌러주세요:")
+class Mode_1:
+
+    def user_input(self, name:str, n_size: int) -> Matrix:
+        mat=Matrix(n_size)
+        print(f"\n[{name} 입력 ({n_size}x{n_size})]")
+        print(f"아래에 {n_size}줄의 데이터를 한 번에 입력(또는 붙여넣기) 후, 엔터를 한 번 더 눌러주세요:")
                 
-        lines = []
+        
         while True :
-            try:
-                line = input().strip()#공백 포함 해서 받음
-                if not line:
+            lines = []
+            while True:
+                try:
+                    line = input().strip()#공백 포함 해서 받음
+                    if not line:
+                        break
+                    lines.append(line) #1줄 입력시 바로 lines에 저장
+                except EOFError:
                     break
-                lines.append(line) #1줄 입력시 바로 lines에 저장
-            except EOFError:
-                break
 
-        if len(lines) != self.n : # 줄 수 검사!
-            print(f"\n 오류 : 입력된 줄 수 ({len(lines)})가 N({self.n})과 맞지 않습니다.")
-            return False
+            if len(lines) != n_size : # 줄 수 검사!
+                print(f"\n 오류 : 입력된 줄 수 ({len(lines)})가 N({n_size})과 맞지 않습니다.")
+                continue
 
-        final_arr = []
-        for i, line in enumerate(lines,1): #각 줄의 '행'가 맞지 않으면 다시
-            row = line.split()
-            if len(row) != self.n:
-                print(f"\n 오류 : {i}번째 줄의 숫자 개수({len(row)}개)가 N({self.n})과 맞지 않습니다.")
-                return False
+            final_arr = []
+            valied = True
+            for i, line in enumerate(lines,1): #각 줄의 '행'가 맞지 않으면 다시
+                row = line.split()
 
-            try:
-                rows = [int(x) for x in row]
-                final_arr.append(rows)
-            except ValueError:
-                print(f"\n오류: {i}번째 숫자가 아닌값이 포함되어 있습니다.")
-                return False
+                if len(row) != n_size:
+                    print(f"\n 오류 : {i}번째 줄의 숫자 개수({len(row)}개)가 N({n_size})과 맞지 않습니다.")
+                    valied =  False
+                    break
 
-        self.data = final_arr
-        print(" 성공적으로 입력을 완료했습니다!")
-        return True
+                try:
+                    rows = [int(x) for x in row]
+                    final_arr.append(rows)
 
+                except ValueError:
+                    print(f"\n오류: {i}번째 숫자가 아닌값이 포함되어 있습니다.")
+                    valied = False
+                    break
 
-    def mac(self, pathern: 'Matrix') -> float | None:
-        if self.n != pathern.n:
-            print(f" 오류 : 행렬 크기가 맞지 않습니다. ({self.n}*{self.n}) VS ({pathern.n}*{pathern.n})")
-            return None
-        if not self.data or not pathern.data:
-            print("오류 : 행렬 데이터가 없습니다. ")
-            return None
+            if valied:
+                mat.data = final_arr
+                print(" 성공적으로 입력을 완료했습니다!")
+                return mat
+            print("다시 입력해주세요! ")
 
-        #제너레이터를 쓰면 0부터 더해서 초기화 필요 X
-        total_sum = sum(
-            self.data[r][c] * pathern.data[r][c]
-            for r in range(self.n)
-            for c in range(self.n)
-        )
-        total_sum= sum(self.data[r][c] * pathern.data[r][c]for r in range(self.n)for c in range(self.n))
-        total_sum = 0.0
-        """
-        for r in range(self.n):
-            for c in range(self.n):
-                total_sum += self.data[r][c] * pathern.data[r][c]"""
+    def mode1_flow(self) -> None:
+        print("\n -----------------[모드 1] 사용자 직접 입력---------------")
+        try:
+            n_size = int(input("행렬 크기(N)를 입력하세요 (예: 3): "))
+            if n_size<=0:
+                print("크기는 1 이상의 양수여야 합니다.")
+                return
+            filter_a = self.user_input("필터 A (Cross)", n_size)
+            filter_b = self.user_input("필터 B (X)", n_size)
+            input_pattern = self.user_input("검사할 패턴", n_size)
 
+            score_a = input_pattern.mac(filter_a)
+            score_b = input_pattern.mac(filter_b)
 
+            print("\n" + "="*40)
+            print(f"필터 A(Cross)와의 Mac 점수 : {score_a}")
+            print(f"필터 B(X)와의 Mac 점수 : {score_b}")
+            print("=" * 40 + "\n")
 
-        return float(total_sum)
-
-    
-
-def mode1():
-    print("\n ------------[모드 1] 사용자 직접 입력 -----------------")
-    try:
-        n_size = int(input("행렬 크기(N)를 입력하세요 (예: 3): "))
-        if n_size <=0:
-            print("크기는 1 이상의 양수여야 합니다!")
-            return
-        filter_a = Matrix(n_size)
-        while True:
-            if filter_a.user_input(name= '필터 A (Cross)'):
-                break
-            print("다시 시도해주세요. \n")
-
-        filter_b = Matrix(n_size)
-        while True:
-            if filter_b.user_input(name= '필터 B (Cross)'):
-                break
-            print("다시 시도해주세요. \n")
-
-        input_pattern = Matrix(n_size)
-        while True:
-            if input_pattern.user_input(name="검사할 패턴"):
-                break
-            print("다시 시도해주세요. \n")
-
-        score_a = input_pattern.mac(filter_a)
-        score_b = input_pattern.mac(filter_b)
-
-        print("\n" + "=" * 40)
-        print(f"필터 A(Cross)와의 Mac 점수 : {score_a}")
-        print(f"필터 B(X)와의 Mac 점수 : {score_b}")
-        print("\n" + "=" * 40)
-    except ValueError:
-        print("올바른 정수를 입력해주세요")
+        except ValueError:
+            print("올바른 정수를 입려하세요.")
 
 
-def run():
-    while True:
-        print("\n===== Mini NPU Simulator =====")
-        print("1. 사용자 직접 입력 모드")
-        print("2. data.json 자동 일괄 분석 모드 (준비 중)")
+        
+class Mode_2():
+    def __init__(self):
+        return 0
+
+
+
+class Manager:
+    def __init__(self):
+        #__init__ 에서 인스턴스를 만들어 이전 실행결과를 기억!
+        self.mode1_run = Mode_1()
+        """self.mode2_run = Mode_2("data.json")"""
+
+    def menu(self) -> None:
+        print("1. 사용자 직접 입력 모드 (Mode1)")
+        print("2. data.json 자동 일괄 분석 모드 (Mode2)")
         print("3. 프로그램 종료")
 
-        choice = input("원하는 모드를 선택하세요(1~3) : ").strip()
+    def run(self) -> None:
+        while True:
+            self.menu()
+            choice = input("원하는 모드를 선택하세요 : ").strip()
 
-        if choice == '1':
-            mode1()
-        elif choice == '2':
-            print("\n 준비중")
-        elif choice == '3':
-            print("\n  준비중")
-            break
-        else:
-            print("\n 올바른 번호를 입력해 주세요 (1, 2, 3).")
-
+            if choice == "1":
+                self.mode1_run.mode1_flow()
+            elif choice == "2":
+                self.mode2_run()
+            elif choice == "3":
+                print("\n프로그램을 종료합니다. 이용해 주셔서 감사합니다!")
+                break
+            else:
+                print("\n 올바른 번호를 입력해 주세요 (1, 2, 3).")
 
 
 if __name__ == "__main__":
-    run()
+    manager = Manager()
+    manager.run()
 
    
     """ try:
--- mode2.py
+++ mode2.py
@@ -1,39 +1,56 @@
 import json
-def json_load(self, file_path:str) -> Dict[str, Any] | None:
-    #함수가 실행을 마치고 반환할 값 / Dict의 키는 항상 str, 값은 리스트 , 상수등 모두 올 수 있음.
-    try:
-        with open(file_path, "r", encoding="utf-8") as f:
-            return json.load(f)
-    except FileNotFoundError:
-        print(f"[오류] {file_path} 파일을 찾을 수 없습니다!")
-    except json.JSONDecodeError:
-        print(f"[오류] {file_path} 파일이 올바른 JSON형식 데이터가 아닙니다.")
-        return None
 
-def size_key(self, pattern_key : str ) -> int | None:
-    # 정규표현식: "size_" 다음에 나오는 숫자를 그룹(\d+)으로 추출
-    match = re.match(r"^size_(\d+)_", pattern_key)
-    #(\d+) 이렇게 괄호로 감싸면 파이썬 정규표현식 엔진이 "이 부분은 나중에 따로 꺼낼 수 있게" '1번 그룹'으로 저장하여 기억!
-    #re.match() : 문자열 처음부터 정규표현식과 맞는지 확인 후 반환
-    if match:
-        return int(match.group(1))
-    #match.group(1)은 정규표현식에서 "첫 번째 괄호 ()로 묶은 부분만 쏙 빼오는 기능
-    return
+class data_json:
 
-# 1. 키 이름에서 크기 N 추출
+    def json_load(self, file_path:str) -> Dict[str, Any] | None:
+        #함수가 실행을 마치고 반환할 값 / Dict의 키는 항상 str, 값은 리스트 , 상수등 모두 올 수 있음.
+        try:
+            with open(file_path, "r", encoding="utf-8") as f:
+                return json.load(f)
+        except FileNotFoundError:
+            print(f"[오류] {file_path} 파일을 찾을 수 없습니다!")
+        except json.JSONDecodeError:
+            print(f"[오류] {file_path} 파일이 올바른 JSON형식 데이터가 아닙니다.")
+            return None
 
-n_size = self._extract_size_from_key(pattern_key)
-if n_size is None:
-    return{
-        "status": "FAIL",
-        "reason": f"패턴 키 명명 규칙 위반 ('size_N_idx' 형식이 아닙니다: '{pattern_key}')"
-    }
 
-# 2. 'size_N'필터가 filters 항목에 존재하는지 확인
-filter_key = f"size_{n_size}"
-if filter_key not in filters:
-    return{
-        "status": "FAIL",
-        "reason": f"대응하는 필터 '{filter_key}'가 filters에 존재하지 않습니다."
-    }
+    """patherns에서 key를 추출 """
+    def extract_size_key(self, pattern_key : str ) -> int | None:
+        # 정규표현식: "size_" 다음에 나오는 숫자를 그룹(\d+)으로 추출
+        match = re.match(r"^size_(\d+)_", pattern_key)
+        #(\d+) 이렇게 괄호로 감싸면 파이썬 정규표현식 엔진이 "이 부분은 나중에 따로 꺼낼 수 있게" '1번 그룹'으로 저장하여 기억!
+        #re.match() : 문자열 처음부터 정규표현식과 맞는지 확인 후 반환
+        if match:
+            return int(match.group(1))
+        #match.group(1)은 정규표현식에서 "첫 번째 괄호 ()로 묶은 부분만 쏙 빼오는 기능
+        return
 
+        # 1. 키 이름에서 크기 N 추출
+        """해당 size_N 필터를 선택!"""
+    
+
+    """필터와 패턴의 크기가 일치하는지검증"""
+
+    def schema_check(self, pattern_key: str, pattern_data: dict, fliters: dict) -> dict:
+        expected = pattern_data.get("expected","UNKNOWN")
+
+        n_size = self.extract_size_key(pattern_key)
+
+        if n_size is None:
+            return{
+                "status": "FAIL",
+                "reason": f"패턴 키 명명 규칙 위반 ('size_N_idx' 형식이 아닙니다: '{pattern_key}')"
+            }
+    
+        # 2. 'size_N'필터가 filters 항목에 존재하는지 확인
+        filter_key = f"size_{n_size}"
+        if filter_key not in filters:
+            return{
+                "status": "FAIL",
+                "reason": f"대응하는 필터 '{filter_key}'가 filters에 존재하지 않습니다."
+            }
+
+        filter_raw = filters[filter_key]
+        input_raw = pattern_data.get("input", [])
+        
+    
